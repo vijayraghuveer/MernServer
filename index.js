51eb0d352f143ae4c13d19c38cc7c62a40e28875
vijayraghuveer <140396051+vijayraghuveer@users.noreply.github.com>
2024-07-23 11:22:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json({extended:true}))
 app.use(express.urlencoded({extended:true}))
-app.use(cors({credentials:true, origin:"http://localhost:3000" }))
+app.use(cors({credentials:true, origin:["http://localhost:3000", "https://vijayraghuveer.github.io"] }))
 app.use(upload())
 app.use('/uploads', express.static(__dirname + '/uploads'))
 
